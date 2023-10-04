92682063ab18a42008dc80d82e452cf8225f41ca
Shenoy Pratik <sgguruda@amazon.com>
2023-10-03 22:35:53 -0700

    merge tableview from main
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- public/components/SQLPage/TableView.tsx
+++ public/components/SQLPage/TableView.tsx
@@ -8,105 +8,139 @@ import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { CoreStart } from '../../../../../src/core/public';
 import { ON_LOAD_QUERY } from '../../../common/constants';
-import { AccelerationIndexFlyout } from './acceleration_index_flyout';
+import { getJobId, pollQueryStatus } from './utils';
 
 interface CustomView {
   http: CoreStart['http'];
-  dataConnection: string;
+  selectedItems: any[];
 }
 
-export const TableView = ({ http, dataConnection }: CustomView) => {
+export const TableView = ({ http, selectedItems }: CustomView) => {
   const [tablenames, setTablenames] = useState<string[]>([]);
   const [selectedNode, setSelectedNode] = useState<string | null>(null);
   const [childData, setChildData] = useState<string[]>([]);
   const [selectedChildNode, setSelectedChildNode] = useState<string | null>(null);
   const [indexData, setIndexedData] = useState<string[]>([]);
-  const [indexFlyout, setIndexFlyout] = useState(<></>);
+  const [isLoading, setIsLoading] = useState(false);
+  const [indiciesData, setIndiciesData] = useState<string[]>([]);
+
+  const get_async_query_results = (id, http, callback) => {
+    pollQueryStatus(id, http, callback);
+  };
 
   const getSidebarContent = () => {
-    const query = { query: ON_LOAD_QUERY };
-    http
-      .post(`/api/sql_console/sqlquery`, {
-        body: JSON.stringify(query),
-      })
-      .then((res) => {
-        const responseObj = res.data.resp ? JSON.parse(res.data.resp) : '';
-        const datarows: any[][] = _.get(responseObj, 'datarows');
-        const fields = datarows.map((data) => {
-          return data[2];
+    if (selectedItems[0].label == 'OpenSearch') {
+      setTablenames([]);
+      const query = { query: ON_LOAD_QUERY };
+      http
+        .post(`/api/sql_console/sqlquery`, {
+          body: JSON.stringify(query),
+        })
+        .then((res) => {
+          const responseObj = res.data.resp ? JSON.parse(res.data.resp) : '';
+          const datarows: any[][] = _.get(responseObj, 'datarows');
+          const fields = datarows.map((data) => {
+            return data[2];
+          });
+          setTablenames(fields);
+        })
+        .catch((err) => {
+          console.error(err);
         });
-        setTablenames(fields);
-      })
-      .catch((err) => {
-        console.error(err);
+    } else {
+      setTablenames([]);
+      const query = {
+        lang: 'sql',
+        query: `SHOW SCHEMAS IN ${selectedItems[0]['label']}`,
+        datasource: selectedItems[0]['label'],
+      };
+      getJobId(query, http, (id) => {
+        get_async_query_results(id, http, (data) => {
+          setTablenames(data);
+        });
       });
+    }
   };
 
   useEffect(() => {
     getSidebarContent();
-  }, []);
+  }, [selectedItems[0]['label']]);
 
   const handleNodeClick = (nodeLabel: string) => {
-    //         // will update after new query
-
-    const newData = ['Child 1', 'Child 2', 'Child 3'];
-    setChildData(newData);
     setSelectedNode(nodeLabel);
+    const query = {
+      lang: 'sql',
+      query: `SHOW TABLES IN ${selectedItems[0]['label']}.${nodeLabel}`,
+      datasource: selectedItems[0]['label'],
+    };
+    getJobId(query, http, (id) => {
+      get_async_query_results(id, http, (data) => {
+        data = data.map((subArray) => subArray[1]);
+        setChildData(data);
+      });
+    });
   };
 
+  const callCoverQuery = (nodeLabel1: string) => {
+    const coverQuery = {
+      lang: 'sql',
+      query: `SHOW INDEX ON ${selectedItems[0]['label']}.${selectedNode}.${nodeLabel1}`,
+      datasource: selectedItems[0]['label'],
+    };
+    getJobId(coverQuery, http, (id) => {
+      get_async_query_results(id, http, (data) => {
+        data = [].concat(...data);
+        indiciesData.push(data);
+        setIndexedData(indiciesData);
+      });
+    });
+  };
   const handleChildClick = (nodeLabel1: string) => {
-    // will update after new query
-
-    const newData1 = ['Child 4', 'Child 5', 'Child 6'];
-    setIndexedData(newData1);
     setSelectedChildNode(nodeLabel1);
+    const skipQuery = {
+      lang: 'sql',
+      query: `DESC SKIPPING INDEX ON ${selectedItems[0]['label']}.${selectedNode}.${nodeLabel1}`,
+      datasource: selectedItems[0]['label'],
+    };
+    getJobId(skipQuery, http, (id) => {
+      get_async_query_results(id, http, (data) => {
+        if (data.length > 0) {
+          indiciesData.push('skip_index');
+          callCoverQuery(nodeLabel1);
+        }
+      });
+    });
   };
 
-  const resetFlyout = () => {
-    setIndexFlyout(<></>);
-  };
-
-  const handleAcclerationIndexClick = (
-    dataSource: string,
-    database: string,
-    dataTable: string,
-    indexChild: string
-  ) => {
-    setIndexFlyout(
-      <AccelerationIndexFlyout
-        dataSource={dataSource}
-        database={database}
-        dataTable={dataTable}
-        indexName={indexChild}
-        resetFlyout={resetFlyout}
-      />
-    );
-  };
-
-  const treeData = tablenames.map((element, index) => ({
-    label: element,
+  const treeData = tablenames.map((database, index) => ({
+    label: <div>{database}</div>,
     icon: <EuiIcon type="database" size="m" />,
     id: 'element_' + index,
-    callback: () => handleNodeClick(element),
+    callback: () => {
+      setChildData([]);
+      setIsLoading(true);
+      handleNodeClick(database);
+    },
     isSelectable: true,
     isExpanded: true,
     children:
-      dataConnection === 'S3' && selectedNode === element
-        ? childData.map((child) => ({
-            label: child,
-            id: `${element}_${child}`,
+      selectedNode === database
+        ? childData.map((table) => ({
+            label: <div>{table}</div>,
+            id: `${database}_${table}`,
             icon: <EuiIcon type="tableDensityCompact" size="s" />,
-            callback: () => handleChildClick(child),
+            callback: () => {
+              setIndexedData([]);
+              handleChildClick(table);
+            },
             sSelectable: true,
             isExpanded: true,
             children:
-              selectedChildNode === child
+              selectedChildNode === table
                 ? indexData.map((indexChild) => ({
                     label: indexChild,
-                    id: `${child}_${indexChild}`,
+                    id: `${table}_${indexChild}`,
                     icon: <EuiIcon type="bolt" size="s" />,
-                    callback: () =>
-                      handleAcclerationIndexClick('myGlue', element, child, indexChild),
                   }))
                 : undefined,
           }))
@@ -124,7 +158,6 @@ export const TableView = ({ http, dataConnection }: CustomView) => {
           title={<h2>Error loading Datasources</h2>}
         />
       )}
-      {indexFlyout}
     </>
   );
 };
