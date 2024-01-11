e98befbd998d0d8a18fc6477c8574ed42401a302
sumukhswamy <sumukhhs@amazon.com>
2024-01-11 14:06:06 -0800

    reverted the tests
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- .cypress/integration/ui.spec.js
+++ .cypress/integration/ui.spec.js
@@ -9,176 +9,176 @@
 import { edit } from 'brace';
 import { delay, files, testDataSet, testQueries, verifyDownloadData } from '../utils/constants';
 
-// describe('Dump test data', () => {
-//   it('Indexes test data for SQL and PPL', () => {
-//     const dumpDataSet = (url, index) =>
-//       cy.request(url).then((response) => {
-//         cy.request({
-//           method: 'POST',
-//           form: false,
-//           url: 'api/console/proxy',
-//           headers: {
-//             'content-type': 'application/json;charset=UTF-8',
-//             'osd-xsrf': true,
-//           },
-//           qs: {
-//             path: `${index}/_bulk`,
-//             method: 'POST',
-//           },
-//           body: response.body,
-//         });
-//       });
+describe('Dump test data', () => {
+  it('Indexes test data for SQL and PPL', () => {
+    const dumpDataSet = (url, index) =>
+      cy.request(url).then((response) => {
+        cy.request({
+          method: 'POST',
+          form: false,
+          url: 'api/console/proxy',
+          headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            'osd-xsrf': true,
+          },
+          qs: {
+            path: `${index}/_bulk`,
+            method: 'POST',
+          },
+          body: response.body,
+        });
+      });
 
-//     testDataSet.forEach(({url, index}) => dumpDataSet(url, index));
-//   });
-// });
+    testDataSet.forEach(({url, index}) => dumpDataSet(url, index));
+  });
+});
 
-// describe('Test PPL UI', () => {
-//   beforeEach(() => {
-//     cy.visit('app/opensearch-query-workbench');
-//     cy.wait(delay);
-//     cy.get('[data-test-subj="PPL"]').click({ force: true });
-//     cy.wait(delay);
-//   });
+describe('Test PPL UI', () => {
+  beforeEach(() => {
+    cy.visit('app/opensearch-query-workbench');
+    cy.wait(delay);
+    cy.get('[data-test-subj="PPL"]').click({ force: true });
+    cy.wait(delay);
+  });
 
-//   it('Test Run button', () => {
-//     cy.get('textarea.ace_text-input').eq(0).focus().type('source=accounts', { force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Run').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiTab__content').contains('Events').click({ force: true });
-//     cy.scrollTo(0, 500) 
+  it('Test Run button', () => {
+    cy.get('textarea.ace_text-input').eq(0).focus().type('source=accounts', { force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Run').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiTab__content').contains('Events').click({ force: true });
+    cy.scrollTo(0, 500) 
     
-//     cy.get('.euiTableRow') 
-//       .contains('Nanette')
-//       .should('exist') 
-//       .then(($element) => {
-//         const text = $element.text(); 
-//     });
-//   });
+    cy.get('.euiTableRow') 
+      .contains('Nanette')
+      .should('exist') 
+      .then(($element) => {
+        const text = $element.text(); 
+    });
+  });
 
-//   it('Test Clear button', () => {
-//     cy.get('textarea.ace_text-input').eq(0).focus().type('search source=.kibana', { force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Run').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiTab__content').contains('Events').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Clear').click({ force: true });
-//     cy.wait(delay);
+  it('Test Clear button', () => {
+    cy.get('textarea.ace_text-input').eq(0).focus().type('search source=.kibana', { force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Run').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiTab__content').contains('Events').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Clear').click({ force: true });
+    cy.wait(delay);
 
-//     cy.get('.euiTextAlign')
-//       .contains('Enter a query in the query editor above to see results.')
-//       .should('have.length', 1);
-//     cy.get('.ace_content')
-//       .eq(0)
-//       .then((queryEditor) => {
-//         const editor = edit(queryEditor[0]);
-//         expect(editor.getValue()).to.equal('');
-//       });
-//   });
+    cy.get('.euiTextAlign')
+      .contains('Enter a query in the query editor above to see results.')
+      .should('have.length', 1);
+    cy.get('.ace_content')
+      .eq(0)
+      .then((queryEditor) => {
+        const editor = edit(queryEditor[0]);
+        expect(editor.getValue()).to.equal('');
+      });
+  });
 
-//   it('Test full screen view', () => {
-//     cy.get('.euiButton__text').contains('Full screen view').should('not.exist');
+  it('Test full screen view', () => {
+    cy.get('.euiButton__text').contains('Full screen view').should('not.exist');
 
-//     cy.get('textarea.ace_text-input').eq(0).focus().type('search source=.kibana', { force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Run').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Full screen view').click({ force: true });
+    cy.get('textarea.ace_text-input').eq(0).focus().type('search source=.kibana', { force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Run').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Full screen view').click({ force: true });
 
-//     cy.get('.euiTitle').should('not.exist');
+    cy.get('.euiTitle').should('not.exist');
 
-//     cy.get('button#exit-fullscreen-button').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Full screen view').should('exist');
-//   });
-// });
+    cy.get('button#exit-fullscreen-button').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Full screen view').should('exist');
+  });
+});
 
-// describe('Test SQL UI', () => {
-//   beforeEach(() => {
-//     cy.visit('app/opensearch-query-workbench');
-//     cy.wait(delay);
-//     cy.get('[data-test-subj="SQL"]').click({ force: true });
-//     cy.wait(delay);
-//   });
+describe('Test SQL UI', () => {
+  beforeEach(() => {
+    cy.visit('app/opensearch-query-workbench');
+    cy.wait(delay);
+    cy.get('[data-test-subj="SQL"]').click({ force: true });
+    cy.wait(delay);
+  });
 
-//   it('Confirm results are empty', () => {
-//     cy.get('.euiTextAlign')
-//       .contains('Enter a query in the query editor above to see results.')
-//       .should('have.length', 1);
-//   });
+  it('Confirm results are empty', () => {
+    cy.get('.euiTextAlign')
+      .contains('Enter a query in the query editor above to see results.')
+      .should('have.length', 1);
+  });
 
-//   it('Test Run button and field search', () => {
-//     cy.get('textarea.ace_text-input')
-//       .eq(0)
-//       .focus()
-//       .type('{enter}', { force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Run').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiTab__content').contains('SHOW tables LIKE \'%\'').click({ force: true });
-//   });
+  it('Test Run button and field search', () => {
+    cy.get('textarea.ace_text-input')
+      .eq(0)
+      .focus()
+      .type('{enter}', { force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Run').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiTab__content').contains('SHOW tables LIKE \'%\'').click({ force: true });
+  });
 
-//   it('Test Translate button', () => {
-//     cy.get('textarea.ace_text-input')
-//       .eq(0)
-//       .focus()
-//       .type('{enter}', {
-//         force: true,
-//       });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Explain').click({ force: true });
-//     cy.wait(delay);
+  it('Test Translate button', () => {
+    cy.get('textarea.ace_text-input')
+      .eq(0)
+      .focus()
+      .type('{enter}', {
+        force: true,
+      });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Explain').click({ force: true });
+    cy.wait(delay);
 
-//     // hard to get euiCodeBlock content, check length instead
-//     cy.get('.euiCodeBlock__code').children().should('have.length', 25);
-//   });
+    // hard to get euiCodeBlock content, check length instead
+    cy.get('.euiCodeBlock__code').children().should('have.length', 25);
+  });
 
-//   it('Test Clear button', () => {
-//     cy.get('.euiButton__text').contains('Clear').click({ force: true });
-//     cy.wait(delay);
+  it('Test Clear button', () => {
+    cy.get('.euiButton__text').contains('Clear').click({ force: true });
+    cy.wait(delay);
 
-//     cy.get('.ace_content')
-//       .eq(0)
-//       .then((queryEditor) => {
-//         const editor = edit(queryEditor[0]);
-//         expect(editor.getValue()).to.equal('');
-//       });
-//   });
+    cy.get('.ace_content')
+      .eq(0)
+      .then((queryEditor) => {
+        const editor = edit(queryEditor[0]);
+        expect(editor.getValue()).to.equal('');
+      });
+  });
 
-//   it('Test full screen view', () => {
-//     cy.get('.euiButton__text').contains('Full screen view').should('not.exist');
+  it('Test full screen view', () => {
+    cy.get('.euiButton__text').contains('Full screen view').should('not.exist');
 
-//     cy.get('.euiButton__text').contains('Run').click({ force: true });
-//     cy.wait(delay);
-//     cy.get('.euiButton__text').contains('Full screen view').click({ force: true });
+    cy.get('.euiButton__text').contains('Run').click({ force: true });
+    cy.wait(delay);
+    cy.get('.euiButton__text').contains('Full screen view').click({ force: true });
 
-//     cy.get('.euiTitle').should('not.exist');
-//   });
-// });
+    cy.get('.euiTitle').should('not.exist');
+  });
+});
 
-// describe('Test and verify SQL downloads', () => {
-//   verifyDownloadData.map(({ title, url, file }) => {
-//     it(title, () => {
-//       cy.request({
-//         method: 'POST',
-//         form: false,
-//         url: url,
-//         headers: {
-//           'content-type': 'application/json;charset=UTF-8',
-//           'osd-xsrf': true,
-//         },
-//         body: {
-//           query:
-//             'select * from accounts where balance > 49500 order by account_number',
-//         },
-//       }).then((response) => {
-//         expect(response.body.data.resp).to.have.string(files[file]);
-//       });
-//     });
-//   });
-// });
+describe('Test and verify SQL downloads', () => {
+  verifyDownloadData.map(({ title, url, file }) => {
+    it(title, () => {
+      cy.request({
+        method: 'POST',
+        form: false,
+        url: url,
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+          'osd-xsrf': true,
+        },
+        body: {
+          query:
+            'select * from accounts where balance > 49500 order by account_number',
+        },
+      }).then((response) => {
+        expect(response.body.data.resp).to.have.string(files[file]);
+      });
+    });
+  });
+});
 
 describe('Test table display', () => {
   beforeEach(() => {
@@ -190,23 +190,23 @@ describe('Test table display', () => {
     cy.wait(delay);
   });
 
-  // testQueries.map(({ title, query, cell_idx, expected_string }) => {
-  //   it(title, () => {
-  //     cy.get('textarea.ace_text-input')
-  //       .eq(0)
-  //       .focus()
-  //       .type(`{selectall}{backspace}${query}`, { force: true });
-  //     cy.wait(delay);
-  //     cy.get('.euiButton__text').contains('Run').click({ force: true });
-  //     cy.wait(delay);
+  testQueries.map(({ title, query, cell_idx, expected_string }) => {
+    it(title, () => {
+      cy.get('textarea.ace_text-input')
+        .eq(0)
+        .focus()
+        .type(`{selectall}{backspace}${query}`, { force: true });
+      cy.wait(delay);
+      cy.get('.euiButton__text').contains('Run').click({ force: true });
+      cy.wait(delay);
 
-  //     cy.get('span.euiTableCellContent__text')
-  //       .eq(cell_idx)
-  //       .should((cell) => {
-  //         expect(cell).to.contain(expected_string);
-  //       });
-  //   });
-  // });
+      cy.get('span.euiTableCellContent__text')
+        .eq(cell_idx)
+        .should((cell) => {
+          expect(cell).to.contain(expected_string);
+        });
+    });
+  });
 
   it('Test nested fields display', () => {
     cy.get('textarea.ace_text-input')
@@ -219,10 +219,8 @@ describe('Test table display', () => {
     cy.get('.euiButton__text').contains('Run').click({ force: true });
     cy.wait(delay);
 
-    // Wait for the button to become clickable before clicking it
-    cy.get('button.euiLink').eq(2).should('be.visible').click({ force: true });
-  
-    // Wait for the content with the text 'message' to appear
-    cy.contains('message').should('exist');
-  });  
+    cy.get('button.euiLink').eq(2).click({ force: true });
+    cy.wait(delay);
+    cy.contains('message');
+  });
 });
