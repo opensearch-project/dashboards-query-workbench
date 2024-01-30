da531e3194ee6b5e15cb4e08ae6ee3e74b0e6cd4
sumukhswamy <sumukhhs@amazon.com>
2024-01-30 15:46:30 -0800

    added jest tests
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- public/components/Main/main.test.tsx
+++ public/components/Main/main.test.tsx
@@ -4,14 +4,16 @@
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import { HttpResponse } from '../../../../../src/core/public';
 import { httpClientMock } from '../../../test/mocks';
 import {
+  mockDataSelectQuery,
   mockDatasourcesQuery,
   mockHttpQuery,
   mockNotOkQueryResultResponse,
+  mockOpenSearchIndicies,
   mockOpenSearchTreeQuery,
   mockQueryResultJDBCResponse,
   mockQueryTranslationResponse,
@@ -30,6 +32,76 @@ describe('<Main /> spec', () => {
       render(<Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />);
     };
     await asyncTest();
+    expect(document.body.children[0]).toMatchSnapshot();
+  });
+  it('renders the component and toggles ppl', async () => {
+    const client = httpClientMock;
+    client.post = jest.fn().mockResolvedValue(mockHttpQuery);
+    client.get = jest.fn().mockResolvedValue(mockDatasourcesQuery);
+    const { getByText } = await render(
+      <Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />
+    );
+    const pplButton = getByText('PPL');
+    const asyncTest = () => {
+      fireEvent.click(pplButton);
+    };
+    waitFor(()=>{
+      asyncTest();
+    })
+    expect(document.body.children[0]).toMatchSnapshot();
+  });
+  it('renders the component and checks if Opensearch is selected', async () => {
+    const client = httpClientMock;
+    client.post = jest.fn().mockResolvedValue(mockHttpQuery);
+    client.get = jest.fn().mockResolvedValue(mockDatasourcesQuery);
+    const { getByText } = await render(
+      <Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />
+    );
+    expect(getByText('OpenSearch')).toBeInTheDocument();
+    expect(document.body.children[0]).toMatchSnapshot();
+  });
+  it('renders the component and s3 glue component is present', async () => {
+    const client = httpClientMock;
+    client.post = jest.fn().mockResolvedValue(mockHttpQuery);
+    client.get = jest.fn().mockResolvedValue(mockDataSelectQuery);
+    const { getByText } = await render(
+      <Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />
+    );
+    expect(getByText('OpenSearch')).toBeInTheDocument();
+    fireEvent.click(getByText('OpenSearch'))
+    await waitFor(() => {
+      expect(getByText('glue_1')).toBeInTheDocument();
+    });
+    expect(document.body.children[0]).toMatchSnapshot();
+  });
+  it('renders the component for s3 glue and check sample query button ', async () => {
+    const client = httpClientMock;
+    client.post = jest.fn().mockResolvedValue(mockHttpQuery);
+    client.get = jest.fn().mockResolvedValue(mockDataSelectQuery);
+    const { getByText } = await render(
+      <Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />
+    );
+    expect(getByText('OpenSearch')).toBeInTheDocument();
+    fireEvent.click(getByText('OpenSearch'))
+    fireEvent.click(getByText('glue_1'))
+    await waitFor(() => {
+      expect(getByText('Sample Query')).toBeInTheDocument();
+    });
+    expect(document.body.children[0]).toMatchSnapshot();
+  });
+  it('renders the component and checks if side tree is loaded', async () => {
+    const client = httpClientMock;
+    client.post = jest.fn().mockResolvedValue(mockHttpQuery);
+    client.get = jest.fn().mockResolvedValue(mockDatasourcesQuery);
+
+    client.post = jest.fn(() => {
+      return (Promise.resolve(mockOpenSearchIndicies) as unknown) as HttpResponse;
+    });
+    const { getByText } = await render(
+      <Main httpClient={client} setBreadcrumbs={setBreadcrumbsMock} />
+    );
+    expect(getByText('.kibana_1')).toBeInTheDocument();
+
     expect(document.body.children[0]).toMatchSnapshot();
   });
 
