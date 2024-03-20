754b93eff2993640ea02f29835a11051b34b5139
Shenoy Pratik <sgguruda@amazon.com>
2024-03-19 18:06:16 -0700

    Updates create accelerations flyout usage and side tree (#285) (#286)
    * init use observability deps
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * update side tree view
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * update tests
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * update release notes
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * removes console.log
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * allow refresh only after first load
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * update snapshots
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * revert version to 3.0
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * on umount stop loading
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    ---------
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    (cherry picked from commit d168db7402d077681d895309ebe05a50e8880e72)

---
+++ public/components/SQLPage/sql_catalog_tree/s3_tree.tsx
@@ -0,0 +1,343 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiLoadingSpinner,
+  EuiSpacer,
+  EuiText,
+  EuiTreeView,
+} from '@elastic/eui';
+import produce from 'immer';
+import React, { useEffect, useState } from 'react';
+import {
+  TREE_ITEM_COVERING_INDEX_DEFAULT_NAME,
+  TREE_ITEM_DATABASE_NAME_DEFAULT_NAME,
+  TREE_ITEM_MATERIALIZED_VIEW_DEFAULT_NAME,
+  TREE_ITEM_SKIPPING_INDEX_DEFAULT_NAME,
+  TREE_ITEM_TABLE_NAME_DEFAULT_NAME,
+} from '../../../../common/constants';
+import { AsyncQueryStatus, CachedDataSourceStatus, TreeItem } from '../../../../common/types';
+import { useToast } from '../../../../common/utils/toast_helper';
+import { getRenderAccelerationDetailsFlyout } from '../../../dependencies/register_observability_dependencies';
+import { catalogCacheRefs } from '../../../framework/catalog_cache_refs';
+import {
+  createLabel,
+  findIndexObject,
+  findMaterializedViewsForDatabase,
+  findSkippingAndCoveringIndexNames,
+  getAccelerationsFromCache,
+  getTablesFromCache,
+  iconCreation,
+  isEitherObjectCacheEmpty,
+  loadTreeItem,
+} from './s3_tree_helpers';
+
+interface S3TreeProps {
+  dataSource: string;
+  updateSQLQueries: (query: string) => void;
+  refreshTree: boolean;
+}
+
+export const S3Tree = ({ dataSource, updateSQLQueries, refreshTree }: S3TreeProps) => {
+  const { setToast } = useToast();
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  const [treeData, setTreeData] = useState<TreeItem[]>([]);
+  const [currentSelectedDatabase, setCurrentSelectedDatabase] = useState('');
+  const renderAccelerationDetailsFlyout = getRenderAccelerationDetailsFlyout();
+  const [isTreeLoading, setIsTreeLoading] = useState({
+    status: false,
+    message: '',
+  });
+  const [isObjectLoading, setIsObjectLoading] = useState({
+    tableStatus: false,
+    accelerationsStatus: false,
+  });
+  const {
+    loadStatus: loadDatabasesStatus,
+    startLoading: startDatabasesLoading,
+    stopLoading: stopDatabasesLoading,
+  } = catalogCacheRefs.useLoadDatabasesToCache();
+  const {
+    loadStatus: loadTablesStatus,
+    startLoading: startLoadingTables,
+    stopLoading: stopLoadingTables,
+  } = catalogCacheRefs.useLoadTablesToCache();
+  const {
+    loadStatus: loadAccelerationsStatus,
+    startLoading: startLoadingAccelerations,
+    stopLoading: stopLoadingAccelerations,
+  } = catalogCacheRefs.useLoadAccelerationsToCache();
+
+  const updateDatabaseState = (databaseName: string, isLoading: boolean, values?: TreeItem[]) => {
+    setTreeData(
+      produce((draft) => {
+        const databaseToUpdate = draft.find((database) => database.name === databaseName);
+        if (databaseToUpdate) {
+          databaseToUpdate.isExpanded = true;
+          databaseToUpdate.isLoading = isLoading;
+          if (values !== undefined) {
+            databaseToUpdate.values = databaseToUpdate.values
+              ? databaseToUpdate.values.concat(values)
+              : values;
+          }
+        }
+      })
+    );
+  };
+
+  const constructObjectTree = (
+    database: string,
+    tablesData: string[],
+    accelerationsData: any[]
+  ) => {
+    const tablesTreeItems = tablesData.map((table) => {
+      const indices = findSkippingAndCoveringIndexNames(accelerationsData, database, table);
+
+      const tableValues = indices.map((index) => {
+        return index === TREE_ITEM_SKIPPING_INDEX_DEFAULT_NAME
+          ? loadTreeItem(
+              [TREE_ITEM_SKIPPING_INDEX_DEFAULT_NAME],
+              TREE_ITEM_SKIPPING_INDEX_DEFAULT_NAME
+            )[0]
+          : loadTreeItem([index], TREE_ITEM_COVERING_INDEX_DEFAULT_NAME)[0];
+      });
+
+      const tableTreeItem = loadTreeItem(
+        [table],
+        TREE_ITEM_TABLE_NAME_DEFAULT_NAME,
+        tableValues
+      )[0];
+      return tableTreeItem;
+    });
+
+    const mvItems = findMaterializedViewsForDatabase(accelerationsData, database);
+    const mvTreeItems = loadTreeItem(mvItems, TREE_ITEM_MATERIALIZED_VIEW_DEFAULT_NAME);
+
+    updateDatabaseState(database, true, [...tablesTreeItems, ...mvTreeItems]);
+  };
+
+  const onClickDatabase = (database: TreeItem) => {
+    if (currentSelectedDatabase === '') {
+      setCurrentSelectedDatabase(database.name);
+      updateDatabaseState(database.name, true);
+      if (!isEitherObjectCacheEmpty(dataSource, database.name)) {
+        const tablesData = getTablesFromCache(dataSource, database.name);
+        const accelerationsData = getAccelerationsFromCache(dataSource);
+
+        constructObjectTree(database.name, tablesData, accelerationsData);
+        updateDatabaseState(database.name, false);
+        setCurrentSelectedDatabase('');
+      } else {
+        updateDatabaseState(database.name, true);
+        setIsObjectLoading({
+          tableStatus: true,
+          accelerationsStatus: true,
+        });
+        startLoadingTables(dataSource, database.name);
+        startLoadingAccelerations(dataSource);
+      }
+    } else {
+      setToast('Can only load one database at a time', 'warning');
+    }
+  };
+
+  const onClickAcceleration = (
+    databaseName: string,
+    tableName: string | undefined,
+    accelerationName: string
+  ) => {
+    const accelerationsData = getAccelerationsFromCache(dataSource);
+    const accelerationObject = findIndexObject(
+      accelerationsData,
+      databaseName,
+      tableName,
+      accelerationName
+    );
+    renderAccelerationDetailsFlyout(accelerationObject, dataSource);
+  };
+
+  const treeDataDatabases = treeData.map((database, index) => ({
+    label: createLabel(database, dataSource, database.name, index, updateSQLQueries),
+    icon: iconCreation(database),
+    id: 'element_' + index,
+    callback: () => {
+      if (database.values?.length === 0) {
+        onClickDatabase(database);
+      }
+    },
+    isSelectable: true,
+    isExpanded: database.isExpanded,
+    children: database.values?.map((table, idx) => ({
+      label: createLabel(table, dataSource, database.name, idx, updateSQLQueries),
+      id: `${database.name}_${table.name}`,
+      icon: iconCreation(table),
+      callback: () => {
+        if (table.type === TREE_ITEM_MATERIALIZED_VIEW_DEFAULT_NAME) {
+          onClickAcceleration(database.name, undefined, table.name);
+        }
+      },
+      isSelectable: true,
+      isExpanded: table.isExpanded,
+      children: table.values?.map((indexChild, idxValue) => ({
+        label: createLabel(indexChild, table.name, idxValue),
+        id: `${database.name}_${table.name}_${indexChild.name}`,
+        icon: iconCreation(indexChild),
+        callback: () => {
+          if (
+            indexChild.type === TREE_ITEM_SKIPPING_INDEX_DEFAULT_NAME ||
+            indexChild.type === TREE_ITEM_COVERING_INDEX_DEFAULT_NAME
+          ) {
+            onClickAcceleration(database.name, table.name, indexChild.name);
+          }
+        },
+      })),
+    })),
+  }));
+
+  const onRefreshTree = () => {
+    setIsTreeLoading({ status: true, message: '' });
+    startDatabasesLoading(dataSource);
+  };
+
+  const onLoadS3Tree = () => {
+    setIsTreeLoading({ status: true, message: '' });
+    const dsCache = catalogCacheRefs.CatalogCacheManager!.getOrCreateDataSource(dataSource);
+
+    if (dsCache.status === CachedDataSourceStatus.Updated && dsCache.databases.length > 0) {
+      const databases = dsCache.databases.map((db) => db.name);
+      setTreeData(loadTreeItem(databases, TREE_ITEM_DATABASE_NAME_DEFAULT_NAME));
+      setIsTreeLoading({ status: false, message: '' });
+    } else if (dsCache.status === CachedDataSourceStatus.Empty) {
+      startDatabasesLoading(dataSource);
+    }
+  };
+
+  useEffect(() => {
+    const status = loadDatabasesStatus.toLowerCase();
+    if (status === AsyncQueryStatus.Success) {
+      setIsTreeLoading({ status: false, message: '' });
+    } else if (status === AsyncQueryStatus.Failed || status === AsyncQueryStatus.Cancelled) {
+      setIsTreeLoading({ status: false, message: 'Failed to load databases' });
+    }
+  }, [loadDatabasesStatus]);
+
+  useEffect(() => {
+    const status = loadTablesStatus.toLowerCase();
+    if (status === AsyncQueryStatus.Success) {
+      setIsObjectLoading({ ...isObjectLoading, tableStatus: false });
+    } else if (status === AsyncQueryStatus.Failed || status === AsyncQueryStatus.Cancelled) {
+      setIsObjectLoading({ ...isObjectLoading, tableStatus: false });
+    }
+  }, [loadTablesStatus]);
+
+  useEffect(() => {
+    const status = loadAccelerationsStatus.toLowerCase();
+    if (status === AsyncQueryStatus.Success) {
+      setIsObjectLoading({ ...isObjectLoading, accelerationsStatus: false });
+    } else if (status === AsyncQueryStatus.Failed || status === AsyncQueryStatus.Cancelled) {
+      setIsObjectLoading({ ...isObjectLoading, accelerationsStatus: false });
+    }
+  }, [loadAccelerationsStatus]);
+
+  useEffect(() => {
+    onLoadS3Tree();
+  }, [dataSource]);
+
+  useEffect(() => {
+    setIsFirstRender(false);
+  }, []);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      return;
+    }
+    // This will only execute on changes to refreshTree after the initial render
+    onRefreshTree();
+  }, [refreshTree]);
+
+  useEffect(() => {
+    if (
+      !(isObjectLoading.accelerationsStatus || isObjectLoading.tableStatus) &&
+      currentSelectedDatabase !== ''
+    ) {
+      const tablesData = getTablesFromCache(dataSource, currentSelectedDatabase);
+      const accelerationsData = getAccelerationsFromCache(dataSource);
+
+      constructObjectTree(currentSelectedDatabase, tablesData, accelerationsData);
+      updateDatabaseState(currentSelectedDatabase, false);
+      setCurrentSelectedDatabase('');
+    }
+  }, [isObjectLoading]);
+
+  useEffect(() => {
+    return () => {
+      stopDatabasesLoading();
+      stopLoadingTables();
+      stopLoadingAccelerations();
+    };
+  }, []);
+
+  const treeLoadingStateRenderer = (
+    <EuiFlexGroup alignItems="center" gutterSize="s" direction="column">
+      <EuiSpacer />
+      <EuiFlexItem>
+        <EuiLoadingSpinner size="l" />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>Loading tree data</EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <div style={{ padding: '10px' }}>
+          <EuiFlexItem>
+            <EuiText textAlign="center" color="subdued">
+              Loading may take over 30 seconds
+            </EuiText>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiText textAlign="center" color="subdued">
+              Status: {loadDatabasesStatus}
+            </EuiText>
+          </EuiFlexItem>
+        </div>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+
+  const treeStateRenderer =
+    isTreeLoading.message === '' ? (
+      <EuiTreeView
+        aria-label="S3 Datasource Folder Tree"
+        data-test-subj="s3-datasource-tree"
+        items={treeDataDatabases}
+      />
+    ) : (
+      <EuiFlexGroup alignItems="center" gutterSize="s" direction="column">
+        <EuiSpacer />
+        <EuiFlexItem>
+          <EuiIcon type="alert" />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>Failed to load the tree</EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <div style={{ padding: '10px' }}>
+            <EuiFlexItem>
+              <EuiText textAlign="center" color="subdued">
+                {isTreeLoading.message}
+              </EuiText>
+            </EuiFlexItem>
+          </div>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+
+  const treeRenderer = (
+    <>
+      {isTreeLoading.status && isTreeLoading.message === ''
+        ? treeLoadingStateRenderer
+        : treeStateRenderer}
+    </>
+  );
+  return <div>{treeRenderer}</div>;
+};
--- release-notes/opensearch-query-workbench.release-notes-2.13.0.0.md
+++ release-notes/opensearch-query-workbench.release-notes-2.13.0.0.md
@@ -2,13 +2,19 @@
 
 Compatible with OpenSearch and OpenSearch Dashboards 2.13.0
 
+### Features
+
+- Updates create accelerations flyout usage and side tree([#285](https://github.com/opensearch-project/dashboards-query-workbench/pull/285))
+
 ### Bug Fixes
+
 - Refactor async calls and minor bug fixes([#274](https://github.com/opensearch-project/dashboards-query-workbench/pull/274))
 
 ### Infrastructure
+
 - Add single version flag during bootstrap to fix version conflicts ([#269](https://github.com/opensearch-project/dashboards-query-workbench/pull/269))
 - Add OSD react as required plugin ([#278](https://github.com/opensearch-project/dashboards-query-workbench/pull/278))
 
 ### Maintenance
-- Add workflow to verify binary install ([#272](https://github.com/opensearch-project/dashboards-query-workbench/pull/272))
 
+- Add workflow to verify binary install ([#272](https://github.com/opensearch-project/dashboards-query-workbench/pull/272))
