6468b38a71ecc438fd061d0d0d65b601f11b77ba
sumukhswamy <sumukhhs@amazon.com>
2024-05-20 15:26:04 -0700

    linter issues
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,15 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
-import { ILegacyClusterClient, IRouter, Logger, OpenSearchServiceSetup } from '../../../../src/core/server';
+import {
+  ILegacyClusterClient,
+  IRouter,
+  Logger,
+  OpenSearchServiceSetup,
+} from '../../../../src/core/server';
 import QueryService from '../services/QueryService';
 import TranslateService from '../services/TranslateService';
 import registerQueryRoute from './query';
 import registerTranslateRoute from './translate';
 
-
-export default function (router: IRouter, client: ILegacyClusterClient | undefined, openSearchServiceSetup: OpenSearchServiceSetup, dataSourceEnabled: boolean, logger: Logger) {
+export default function (
+  router: IRouter,
+  client: ILegacyClusterClient | undefined,
+  openSearchServiceSetup: OpenSearchServiceSetup,
+  dataSourceEnabled: boolean,
+  logger: Logger
+) {
   const translateService = new TranslateService(client, dataSourceEnabled, logger);
   registerTranslateRoute(router, translateService, openSearchServiceSetup);
 
