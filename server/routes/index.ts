1202c0cf9ff796fae2e3905fa7edf43f5aaeae82
sumukhswamy <sumukhhs@amazon.com>
2024-05-21 10:14:15 -0700

    linting fix
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -14,7 +14,7 @@ import TranslateService from '../services/TranslateService';
 import registerQueryRoute from './query';
 import registerTranslateRoute from './translate';
 
-export default function defineRoutes(
+export default function servicesRouter (
   router: IRouter,
   client: ILegacyClusterClient | undefined,
   openSearchServiceSetup: OpenSearchServiceSetup,
